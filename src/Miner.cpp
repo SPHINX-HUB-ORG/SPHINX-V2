d1f1d5376155bbd9db81125074efc9ae7f27ce6e
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-10 00:11:34 +0700

    Update Miner.cpp
    improvement
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Miner.cpp
+++ src/Miner.cpp
@@ -3,25 +3,46 @@
 // This software is distributed under the MIT License.
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// The given code represents a Miner class in the SPHINXMiner namespace, which is responsible for mining blocks by finding valid proof-of-work. 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The given code represents a Miner class in the SPHINXMiner namespace, which is responsible for mining blocks by finding valid 
+// proof-of-work. 
 
-    // Miner::Miner(): This is the constructor for the Miner class. It initializes the member variables difficulty_, rewardHalvingInterval_, and reward_ with their respective values. Additionally, it creates an instance of the SPHINXAsset::AssetManager class and issues the genesis block reward by calling the issueSPX function with the appropriate parameters.
+    // Miner::Miner(): This is the constructor for the Miner class. It initializes the member variables difficulty_, 
+    // rewardHalvingInterval_, and reward_ with their respective values. Additionally, it creates an instance of the 
+    // SPHINXAsset::AssetManager class and issues the genesis block reward by calling the issueSPX function with the appropriate 
+    // parameters.
 
-    // Block Miner::mineBlock(const std::string& previousHash, const std::string& rewardAddress): This function is responsible for mining a new block. It takes the previousHash and rewardAddress as parameters. It initializes a blockData string by concatenating the previousHash and rewardAddress. It then enters a loop where it generates a hash by concatenating the blockData with a nonce value. The hash is checked for the proof-of-work requirement by comparing the first difficulty_ characters with the required number of leading zeros. If the proof-of-work requirement is met, a reward is issued to the miner using the issueSPX function from the SPHINXAsset::AssetManager class. The loop breaks once a valid proof-of-work is found. Finally, a new Block object is created, and the reward transaction is added to it along with the current timestamp.
+    // Block Miner::mineBlock(const std::string& previousHash, const std::string& rewardAddress): This function is responsible for 
+    // mining a new block. It takes the previousHash and rewardAddress as parameters. It initializes a blockData string by concatenating 
+    // the previousHash and rewardAddress. It then enters a loop where it generates a hash by concatenating the blockData with a nonce 
+    // value. The hash is checked for the proof-of-work requirement by comparing the first difficulty_ characters with the required 
+    // number of leading zeros. If the proof-of-work requirement is met, a reward is issued to the miner using the issueSPX function from
+    // the SPHINXAsset::AssetManager class. The loop breaks once a valid proof-of-work is found. Finally, a new Block object is created, 
+    // and the reward transaction is added to it along with the current timestamp.
 
-    // std::string Miner::calculateProofOfWork(const std::string& blockData, int difficulty): This function calculates the proof-of-work for a given blockData string and difficulty level. It repeatedly appends a "nonce" string to the blockData and calculates the hash using the SPHINXHash::SPHINX_256 function from the Hash.hpp file. It checks if the resulting proof-of-work satisfies the difficulty requirement (leading zeros), and once it does, it returns the proof-of-work string.
+    // std::string Miner::calculateProofOfWork(const std::string& blockData, int difficulty): This function calculates the 
+    // proof-of-work for a given blockData string and difficulty level. It repeatedly appends a "nonce" string to the blockData and 
+    // calculates the hash using the SPHINXHash::SPHINX_256 function from the Hash.hpp file. It checks if the resulting proof-of-work 
+    // satisfies the difficulty requirement (leading zeros), and once it does, it returns the proof-of-work string.
 
-    // void Miner::performMining(): This function simulates the mining process. It contains two scenarios: the developer mining phase and the normal mining phase. It initializes variables such as totalBlocks, developerBlocks, minedBlocks, developerMinedBlocks, developerMining, blockData, rewardAddress, and nonces with their respective values.
+    // void Miner::performMining(): This function simulates the mining process. It contains two scenarios: the developer mining phase and
+    // the normal mining phase. It initializes variables such as totalBlocks, developerBlocks, minedBlocks, developerMinedBlocks, 
+    // developerMining, blockData, rewardAddress, and nonces with their respective values.
 
-    // In the developer mining phase, the function enters a loop where it generates hashes with different nonce values and checks for valid proof-of-work. If a valid proof-of-work is found, a reward is issued to the miner, and the necessary counters are incremented. Once the required number of developer blocks is mined, the function transitions to the normal mining phase.
+    // In the developer mining phase, the function enters a loop where it generates hashes with different nonce values and checks for 
+    // valid proof-of-work. If a valid proof-of-work is found, a reward is issued to the miner, and the necessary counters are incremented.
+    // Once the required number of developer blocks is mined, the function transitions to the normal mining phase.
 
-    // In the normal mining phase, the function follows a similar loop, generating hashes and checking for valid proof-of-work. If a valid proof-of-work is found, a reward is issued, and the necessary counters are updated. The loop continues until the total number of blocks is mined.
+    // In the normal mining phase, the function follows a similar loop, generating hashes and checking for valid proof-of-work. If a valid
+    // proof-of-work is found, a reward is issued, and the necessary counters are updated. The loop continues until the total number of 
+    // blocks is mined.
 
-    // void Miner::updateReward(): This function checks if the current block height is at a reward halving interval (determined by rewardHalvingInterval_). If the condition is met, the reward is halved by dividing it by 2.
+    // void Miner::updateReward(): This function checks if the current block height is at a reward halving interval (determined by 
+    // rewardHalvingInterval_). If the condition is met, the reward is halved by dividing it by 2.
 
-// This functions defined in Miner.cpp can create an instance of the Miner class, call the mineBlock function to mine new blocks, and perform the mining process using the performMining function.
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This functions defined in Miner.cpp can create an instance of the Miner class, call the mineBlock function to mine new blocks, and 
+// perform the mining process using the performMining function.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #include <chrono>
