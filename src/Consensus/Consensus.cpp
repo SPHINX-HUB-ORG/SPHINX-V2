e190a8272ba969f7609d429b44f78c97b68d9a03
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-10 00:23:00 +0700

    Update Consensus.cpp
    improvement
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Consensus/Consensus.cpp
+++ src/Consensus/Consensus.cpp
@@ -3,25 +3,36 @@
 // This software is distributed under the MIT License.
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// The provided code represents a namespace SPHINXConsensus containing the implementation of the Consensus class. Here's an explanation of the code:
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The provided code represents a namespace SPHINXConsensus containing the implementation of the Consensus class. Here's an explanation of 
+// the code:
 
-// The code includes the headers "Consensus.hpp" and "Utils.hpp", which likely contain necessary definitions and declarations for the Consensus class and utility functions.
+// The code includes the headers "Consensus.hpp" and "Utils.hpp", which likely contain necessary definitions and declarations for the 
+// Consensus class and utility functions.
 
-// The code defines the Consensus class constructor inside the namespace SPHINXConsensus. The constructor implementation is not shown in the provided code snippet.
+// The code defines the Consensus class constructor inside the namespace SPHINXConsensus. The constructor implementation is not shown in 
+// the provided code snippet.
 
-// The addVerifiedTransaction function takes a Transaction object as a parameter and adds it to the list of verified transactions. The function simply pushes the transaction to the verifiedTransactions vector.
+// The addVerifiedTransaction function takes a Transaction object as a parameter and adds it to the list of verified transactions. 
+// The function simply pushes the transaction to the verifiedTransactions vector.
 
-// The validateAndAddTransaction function takes a Transaction object as a parameter and performs various validation checks on the transaction using utility functions from SPHINXUtils. If the transaction passes all the validation checks, it proceeds with the consensus algorithm to ensure agreement on its validity.
+// The validateAndAddTransaction function takes a Transaction object as a parameter and performs various validation checks on the 
+// transaction using utility functions from SPHINXUtils. If the transaction passes all the validation checks, it proceeds with the 
+// consensus algorithm to ensure agreement on its validity.
 
 // If the transaction is deemed valid, it is added to the list of verified transactions by calling the addVerifiedTransaction function.
 
-// Additionally, the code creates an instance of the SPHINXContract::SmartContract class, passing 100 as a parameter to its constructor. It then calls the storeTransaction function from "contract.hpp" to write the agreement. The purpose and details of these functions are not provided in the code snippet.
+// Additionally, the code creates an instance of the SPHINXContract::SmartContract class, passing 100 as a parameter to its constructor. 
+// It then calls the storeTransaction function from "contract.hpp" to write the agreement. The purpose and details of these functions are 
+// not provided in the code snippet.
 
-// The code suggests that there are other consensus-related functions that are not shown in the provided snippet. These additional functions likely contribute to the consensus algorithm and its functioning.
+// The code suggests that there are other consensus-related functions that are not shown in the provided snippet. These additional 
+// functions likely contribute to the consensus algorithm and its functioning.
 
-// In summary, the code represents a consensus mechanism where transactions are validated, added to the list of verified transactions, and processed through a consensus algorithm. If the transaction is agreed upon as valid, it is stored as an agreement using the SPHINXContract::SmartContract class.
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// In summary, the code represents a consensus mechanism where transactions are validated, added to the list of verified transactions, 
+// and processed through a consensus algorithm. If the transaction is agreed upon as valid, it is stored as an agreement using the 
+// SPHINXContract::SmartContract class.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #include <iostream>
