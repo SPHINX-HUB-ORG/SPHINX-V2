61c99808df34a6685156525562a6faee222441c7
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-10 00:01:53 +0700

    Update base58.h
    improve
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/base58.h
+++ src/base58.h
@@ -2,14 +2,15 @@
 // All rights reserved.
 // This software is distributed under the MIT License.
 
-
-// Satoshi Nakamoto
+//**
+// by Satoshi Nakamoto
 // Why base-58 instead of standard base-64 encoding?
 // - Don't want 0OIl characters that look the same in some fonts and
 //      could be used to create visually identical looking account numbers.
 // - A string with non-alphanumeric characters is not as easily accepted as an account number.
 // - E-mail usually won't line-break if there's no punctuation to break at.
 // - Doubleclicking selects the whole number as one word if it's all alphanumeric.
+//**
 
 #ifndef LIBBASE58_H
 #define LIBBASE58_H
