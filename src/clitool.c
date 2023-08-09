179d3da4ddf01b0b966ec535a82d537888662d9f
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-10 00:03:15 +0700

    Update clitool.c
    improve
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/clitool.c
+++ src/clitool.c
@@ -1,11 +1,16 @@
-// Satoshi Nakamoto
+// Copyright (c) [2023] SPHINX-HUB
+// All rights reserved.
+// This software is distributed under the MIT License.
+
+//**
+// by Satoshi Nakamoto
 // Why base-58 instead of standard base-64 encoding?
 // - Don't want 0OIl characters that look the same in some fonts and
 //      could be used to create visually identical looking account numbers.
 // - A string with non-alphanumeric characters is not as easily accepted as an account number.
 // - E-mail usually won't line-break if there's no punctuation to break at.
 // - Doubleclicking selects the whole number as one word if it's all alphanumeric.
-
+//**
 
 
 #include <ctype.h>
