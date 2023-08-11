314488949d70026dd93d325b2ace487bcc54cae5
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-11 20:59:24 +0700

    Update Mempool.hpp
    Replace into header file
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Mempool.hpp
+++ src/Mempool.hpp
@@ -3,41 +3,65 @@
 // This software is distributed under the MIT License.
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// The code defines a class Mempool within the SPHINXMempool namespace. This class represents a mempool (memory pool) and provides functionality related to transaction handling and validation.
-
-  // The Mempool class has a private member variable common of type Common&, which is a reference to an instance of the Common class. This reference is initialized in the constructor of the Mempool class.
-
-  // The broadcastTransaction function is responsible for broadcasting a transaction to the mempool. It takes a Transaction object as input. Inside the function, the transaction is first added to the mempool by calling the processMempool function of the common object.
-
-  // Based on the type of the transaction, the function then calls the relevant validation function from the Contract::Mempool namespace. The validation functions are specific to different transaction types (e.g., asset creation, minting, burning, transfer, member addition, and member removal). The appropriate validation function is called by dynamically casting the Transaction object to the corresponding derived transaction type.
-
-  // The code assumes that the relevant validation functions are implemented within the Contract::Mempool namespace.
-
-  // The code does not provide the implementation of the constructor or other mempool-related functions, which are omitted and marked as "// Other mempool-related functions...". These functions would be implemented to handle other mempool operations such as transaction removal, mempool size management, etc.
-
-// The Mempool class serves as a component for managing the mempool and validating different types of transactions before they are added to the mempool for further processing.
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-#ifndef MEMPOOL_HPP
-#define MEMPOOL_HPP
-
-#pragma once
+#ifndef SPHINX_MEMPOOL_HPP
+#define SPHINX_MEMPOOL_HPP
 
 #include <iostream>
+#include <string>
 #include <vector>
-#include "Common.hpp"
-#include "Transaction.hpp"
-#include "Consensus/Contract.hpp"
-#include "Node.hpp"
-#include "Checksum.hpp"
-#include "Verify.hpp"
-#include "json.hh"
+#include <stdexcept>
+#include "json.hpp"
 
+namespace SPHINXTrx {
+
+    enum class TransactionType {
+        ASSET_CREATION,
+        MINTING,
+        BURNING,
+        TRANSFER,
+        MEMBER_ADDITION,
+        MEMBER_REMOVAL
+    };
+
+    class Transaction {
+        // Define Transaction class members and methods...
+    };
+
+    class AssetCreationTransaction : public Transaction {
+        // Define AssetCreationTransaction class members and methods...
+    };
+
+    class MintingTransaction : public Transaction {
+        // Define MintingTransaction class members and methods...
+    };
+
+    // Define other transaction classes...
+
+} // namespace SPHINXTrx
+
+namespace SPHINXCommon {
+
+    class Common {
+    public:
+        void processMempool(const SPHINXTrx::Transaction& transaction) {
+            // Implement mempool processing logic...
+        }
+    };
+
+} // namespace SPHINXCommon
+
+namespace SPHINXNode {
+
+    void processTransaction(const std::string& node, const std::string& transactionData) {
+        // Implement node transaction processing logic...
+    }
+
+} // namespace SPHINXNode
 
 namespace SPHINXMempool {
+
+    using namespace SPHINXTrx;
+    using json = nlohmann::json;
 
     class SPHINXMempool {
     private:
@@ -48,94 +72,10 @@ namespace SPHINXMempool {
             // Constructor implementation...
         }
 
-        void broadcastTransaction(const Transaction& transaction) {
-            // Add the transaction to the mempool
-            common.processMempool(transaction);
-
-            // Call the relevant validation function based on the transaction type
-            if (transaction.getType() == TransactionType::ASSET_CREATION) {
-                const AssetCreationTransaction& assetCreationTx = dynamic_cast<const AssetCreationTransaction&>(transaction);
-                SPHINXMempool::validateAssetCreation(assetCreationTx.getContractAddress(), assetCreationTx.getName(), assetCreationTx.getInitialSupply());
-            } else if (transaction.getType() == TransactionType::MINTING) {
-                const MintingTransaction& mintingTx = dynamic_cast<const MintingTransaction&>(transaction);
-                SPHINXMempool::validateMinting(mintingTx.getAddress(), mintingTx.getName(), mintingTx.getAmount());
-            } else if (transaction.getType() == TransactionType::BURNING) {
-                const BurningTransaction& burningTx = dynamic_cast<const BurningTransaction&>(transaction);
-                SPHINXMempool::validateBurning(burningTx.getAddress(), burningTx.getName(), burningTx.getAmount());
-            } else if (transaction.getType() == TransactionType::TRANSFER) {
-                const TransferTransaction& transferTx = dynamic_cast<const TransferTransaction&>(transaction);
-                SPHINXMempool::validateTransfer(transferTx.getFrom(), transferTx.getTo(), transferTx.getName(), transferTx.getAmount());
-            } else if (transaction.getType() == TransactionType::MEMBER_ADDITION) {
-                const MemberAdditionTransaction& memberAdditionTx = dynamic_cast<const MemberAdditionTransaction&>(transaction);
-                SPHINXMempool::validateMemberAddition(memberAdditionTx.getMemberAddress());
-            } else if (transaction.getType() == TransactionType::MEMBER_REMOVAL) {
-                const MemberRemovalTransaction& memberRemovalTx = dynamic_cast<const MemberRemovalTransaction&>(transaction);
-                SPHINXMempool::validateMemberRemoval(memberRemovalTx.getMemberAddress());
-            }
-        }
-
-        void processTransaction(const std::string& transactionHash) {
-            std::cout << "Processing transaction in the mempool: " << transactionHash << std::endl;
-            // Perform necessary operations on the transaction in the mempool
-        }
-
-        void Mempool::addTransaction(const SPHINXTrx::Transaction& transaction) {
-            // Convert the transaction to JSON
-            std::string transactionJson = transaction.toJson().dump();
-            
-            // Add the transaction to the mempool
-            // Your implementation here
-            // For example, you can store the transaction in a container or process it immediately.
-            processTransaction(transactionJson);
-        }
-
-        void broadcastToNodes(const std::vector<std::string>& nodes, const std::string& transactionData) {
-            std::cout << "Broadcasting transaction to nodes:" << std::endl;
-            for (const auto& node : nodes) {
-                std::cout << "Node: " << node << ", Transaction: " << transactionData << std::endl;
-                // Send the transaction data to each node for further processing
-                SPHINXNode::processTransaction(node, transactionData);
-            }
-        }
-
-        // Implementation of validation functions
-
-        void validateAssetCreation(const std::string& contractAddress, const std::string& name, int initialSupply) {
-            // Implement the validation logic for asset creation using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
-
-        void validateMinting(const std::string& address, const std::string& name, int amount) {
-            // Implement the validation logic for minting using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
-
-        void validateBurning(const std::string& address, const std::string& name, int amount) {
-            // Implement the validation logic for burning using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
-
-        void validateTransfer(const std::string& from, const std::string& to, const std::string& name, int amount) {
-            // Implement the validation logic for transfer using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
-
-        void validateMemberAddition(const std::string& memberAddress) {
-            // Implement the validation logic for member addition using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
-
-        void validateMemberRemoval(const std::string& memberAddress) {
-            // Implement the validation logic for member removal using the provided parameters
-            // You can use the SPHINXCheck and SPHINXVerify classes here
-        }
+        // Rest of the class implementation...
 
     };
 
 } // namespace SPHINXMempool
 
-#endif //SPHINX_MEMPOOL_HPP
-
-
-
-
+#endif // SPHINX_MEMPOOL_HPP
