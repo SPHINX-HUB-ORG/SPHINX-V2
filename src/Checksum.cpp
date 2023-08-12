ed87d386a0d311322bfd4646cba55b2834514e25
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-12 20:38:28 +0700

    Update Checksum.cpp
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Checksum.cpp
+++ src/Checksum.cpp
@@ -3,28 +3,42 @@
 // This software is distributed under the MIT License.
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// The provided code belongs to the SPHINXCheck namespace and includes a class called Checksum. Let's understand the class and its functions:
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The provided code belongs to the SPHINXCheck namespace and includes a class called Checksum. Let's understand the class and its 
+// functions:
 
 // Class Checksum and its Public Member Functions:
-    // bool validateAddress(const std::string& address): This is the primary function that validates the given address. It performs the following steps:
-    // Calls the private member function isAddressValidLength to check if the address is of the correct length (64 characters). If not, it immediately returns false, indicating an invalid address.
-    // Calls the private member function isAddressValidChecksum to verify if the address's checksum is valid. It uses the Base58Check decoding to retrieve the original data and then calculates a checksum based on the data using SPHINXKey::sphinxHash256 function from "Key.cpp." It then compares the calculated checksum with the provided checksum. If they don't match, it returns false, indicating an invalid address.
-    // Calls the private member function isDigitalSignatureValid to check if the digital signature of the address is valid. This part is currently a placeholder (as indicated by the TODO comment) and requires additional implementation for the actual digital signature validation. For now, it always returns true.
-    // If all the checks pass, the function returns true, indicating a valid address.
+    // bool validateAddress(const std::string& address): This is the primary function that validates the given address. It performs the 
+    // following steps:
+        // Calls the private member function isAddressValidLength to check if the address is of the correct length (64 characters). 
+        // If not, it immediately returns false, indicating an invalid address.
+        // Calls the private member function isAddressValidChecksum to verify if the address's checksum is valid. It uses the Base58Check 
+        // decoding to retrieve the original data and then calculates a checksum based on the data using SPHINXKey::sphinxHash256 function
+        // from "Key.cpp." It then compares the calculated checksum with the provided checksum. If they don't match, it returns false, 
+        // indicating an invalid address.
+        // Calls the private member function isDigitalSignatureValid to check if the digital signature of the address is valid. This part
+        // is currently a placeholder (as indicated by the TODO comment) and requires additional implementation for the actual digital 
+        // signature validation. For now, it always returns true.
+        // If all the checks pass, the function returns true, indicating a valid address.
 
 // Private Member Functions:
-    // bool isAddressValidLength(const std::string& address): This function checks if the given address is of the correct length. It returns true if the address length is 64 characters; otherwise, it returns false.
-    // bool isAddressValidChecksum(const std::string& address): This function validates the checksum of the address. It performs the following steps:
-    // Extracts the last 8 characters from the address, which represent the checksum.
-    // Decodes the Base58Check address to retrieve the original data.
-    // Calculates the checksum using the SPHINXKey::sphinxHash256 function from "Key.cpp" by excluding the last 4 bytes (checksum) from the decoded data.
-    // Encodes the calculated hash as Base58 and retrieves the first 4 characters as the calculated checksum.
-    // Compares the calculated checksum with the provided checksum and returns true if they match; otherwise, it returns false.
-    // bool isDigitalSignatureValid(const std::string& address): This function currently serves as a placeholder for validating the digital signature of the address. It extracts the signature from the address, decodes it using Base58Check, and returns true. However, this part is not fully implemented and should be customized to perform the actual digital signature validation.
+    // bool isAddressValidLength(const std::string& address): This function checks if the given address is of the correct length. It returns
+    // true if the address length is 64 characters; otherwise, it returns false.
+    // bool isAddressValidChecksum(const std::string& address): This function validates the checksum of the address. It performs the 
+    // following steps:
+        // Extracts the last 8 characters from the address, which represent the checksum.
+        // Decodes the Base58Check address to retrieve the original data.
+        // Calculates the checksum using the SPHINXKey::sphinxHash256 function from "Key.cpp" by excluding the last 4 bytes (checksum) 
+        // from the decoded data.
+        // Encodes the calculated hash as Base58 and retrieves the first 4 characters as the calculated checksum.
+        // Compares the calculated checksum with the provided checksum and returns true if they match; otherwise, it returns false.
+        // bool isDigitalSignatureValid(const std::string& address): This function currently serves as a placeholder for validating the 
+        // digital signature of the address. It extracts the signature from the address, decodes it using Base58Check, and returns true. 
+        // However, this part is not fully implemented and should be customized to perform the actual digital signature validation.
 
-// Note that the code provided is a simplified version and may require additional implementation details for the Base58Check class, digital signature validation, and other functionalities related to address validation in blockchain systems.
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Note that the code provided is a simplified version and may require additional implementation details for the Base58Check class, 
+// digital signature validation, and other functionalities related to address validation in blockchain systems.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
