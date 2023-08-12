295e4588eeaf528d5f4a61e78b46da7210067fbd
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-12 20:30:01 +0700

    Update Chainstate.cpp
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Chainstate.cpp
+++ src/Chainstate.cpp
@@ -3,35 +3,110 @@
 // This software is distributed under the MIT License.
 
 
-#include "chainstate.hpp" // Include necessary headers
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream> // Include the necessary header for cout
+
+#include "Chainstate.hpp" // Include necessary headers
+#include "Fees.hpp"
+#include "Block.cpp"
+#include "Chain.hpp"
+#include "Consensus/Contract.hpp"
+
 
 namespace ChainState {
 
     // Define data structures to represent the blockchain state
-    // ...
+    struct Account {
+        std::string address;
+        uint64_t balance;
+    };
+
+    struct Asset {
+        std::string assetId;
+        std::string ownerAddress;
+        // Add more properties as needed
+    };
+
+    struct SmartContract {
+        std::string contractName;
+        // Add more properties and methods for the smart contract
+        bool matchesTransaction(const SPHINXFees::Transaction& transaction) const {
+            // Implement the logic to determine if this contract matches the transaction
+        }
+        void execute(const SPHINXFees::Transaction& transaction) {
+            // Implement the logic to execute the smart contract based on the transaction
+        }
+    };
+
+    // List to store smart contracts
+    std::vector<SmartContract> smartContracts;
 
     // Initialize the initial state of the blockchain
     void initializeChainstate() {
-        // Initialize accounts, contracts, etc.
+        // Create a default account with address "0x0" and balance 0
+        Account account;
+        account.address = "0x0";
+        account.balance = 0;
+
+        // Add the default account to the blockchain state
+        SPHINXChain::blockchain.addAccount(account);
+
+        // Add the "SPHINXContract" smart contract to the list of smart contracts
+        SmartContract sphinxContract;
+        sphinxContract.contractName = "SPHINXContract";
+        // Initialize other properties and methods for the contract
+        smartContracts.push_back(sphinxContract);
     }
 
     // Process a new block and update the blockchain state
-    void processBlock(const Block& block) {
+    void processBlock(const SPHINXBlock::Block& block) {
         // Extract transactions from the block
-        std::vector<std::string> transactions = block.getTransactions();
+        std::vector<SPHINXFees::Transaction> transactions = block.getTransactions();
 
         // Process each transaction and update the state accordingly
-        for (const std::string& transaction : transactions) {
+        for (const SPHINXFees::Transaction& transaction : transactions) {
             // Update account balances, execute smart contracts, etc.
+            addBalance(transaction.recipientAddress, transaction.amount);
+            addBalance("miner_reward_address", transaction.transactionFee);
+
+            // Execute smart contracts and update the state
+            executeSmartContracts(transaction);
+
+            // Modify the blockchain state based on the transaction
         }
     }
 
-    // Query the current state of the blockchain
-    uint64_t getAccountBalance(std::string address) {
-        // Retrieve account balance from the chainstate
+    // Record ownership of assets
+    void recordAssetOwnership(const Asset& asset) {
+        // Add the asset record to the blockchain state
+        SPHINXChain::blockchain.addAsset(asset);
     }
 
-    // Other functions to update and query the blockchain state
-    // ...
+    // Execute smart contracts and update the state
+    void executeSmartContracts(const SPHINXFees::Transaction& transaction) {
+        for (const SmartContract& contract : smartContracts) {
+            if (contract.matchesTransaction(transaction)) {
+                contract.execute(transaction);
+            }
+        }
+    }
 
+    // Audit the blockchain
+    void auditBlockchain() {
+        // Iterate through the blocks in the blockchain
+        for (const SPHINXBlock::Block& block : SPHINXChain::blockchain.getBlocks()) {
+            // Process each transaction in the block and verify its validity
+            const std::vector<SPHINXFees::Transaction>& transactions = block.getTransactions();
+            for (const SPHINXFees::Transaction& transaction : transactions) {
+                // Verify the transaction's validity and authorization
+                if (isTransactionValid(transaction)) {
+                    // Transaction is valid
+                } else {
+                    // Transaction is invalid
+                }
+            }
+        }
+    }
 } // namespace ChainState
