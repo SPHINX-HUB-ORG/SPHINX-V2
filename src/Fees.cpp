ac145504c4f674face8fe80bb0ffa54f44db8f3e
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-12 19:32:08 +0700

    Update Fees.cpp
    add code
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Fees.cpp
+++ src/Fees.cpp
@@ -1,3 +1,69 @@
 // Copyright (c) [2023] SPHINX-HUB
 // All rights reserved.
 // This software is distributed under the MIT License.
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace SPHINXFees {
+
+    // Define a basic transaction structure
+    struct Transaction {
+        std::string from;
+        std::string to;
+        double amount;
+        double gasPrice;
+        double gasLimit;
+    };
+
+    // Define a function to calculate transaction fees
+    double calculateTransactionFee(const Transaction& tx) {
+        // Custom fee logic: Lower fees for smaller transactions
+        double baseFee = 0.005; // Base fee per gas unit
+        double transactionFee = baseFee * tx.gasLimit;
+
+        // Lower fee for smaller transactions
+        if (tx.amount <= 10) {
+            transactionFee *= 0.8; // 80% fee reduction for small transactions
+        }
+
+        return transactionFee;
+    }
+
+    // Define a function to process transactions and validate blocks
+    void processTransactions(const std::vector<Transaction>& transactions) {
+        // Simulate block processing
+        for (const Transaction& tx : transactions) {
+            double transactionFee = calculateTransactionFee(tx);
+            std::cout << "Processing transaction: "
+                      << tx.from << " -> " << tx.to
+                      << " Amount: " << tx.amount
+                      << " Fee: " << transactionFee << std::endl;
+
+            // Check if sender has enough balance to cover the fee
+            // and other validation logic
+
+            // Deduct fees from sender's balance
+
+            // Update balances and blockchain state
+        }
+    }
+
+}
+
+int main() {
+    // Create sample transactions
+    std::vector<SPHINXFees::Transaction> transactions = {
+        {"Alice", "Bob", 10.0, 0.01, 100},
+        {"Charlie", "David", 5.0, 0.02, 150},
+        // Add more transactions here
+    };
+
+    // Process transactions and validate blocks
+    SPHINXFees::processTransactions(transactions);
+
+    return 0;
+}
