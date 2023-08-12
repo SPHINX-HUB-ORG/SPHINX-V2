bd49efa957f468c621d22d05098d48c11e3cf94d
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-12 20:04:58 +0700

    Update PoW.hpp
    improvement
    
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/PoW.hpp
+++ src/PoW.hpp
@@ -19,32 +19,19 @@
 
 namespace SPHINXMiningAlgorithm {
     std::string calculateHash(const std::string& message, int requiredLeadingZeros);
-
     bool meetsDifficultyTarget(const std::string& hash, int requiredLeadingZeros);
-
-    std::string calculateHash(const std::string& message, int requiredLeadingZeros);
-
-    // Define SPHINXHash::SPHINX_256 here if not defined
-
-} // namespace SPHINXMiningAlgorithm
+}
 
 namespace SPHINXPoW {
-    std::string solveNonce(const std::string& data, int difficulty);
-
     bool meetsDifficultyTarget(const std::string& hash, int difficulty);
-
     void adjustDifficulty(double& target, double networkHashRate, bool developerMining);
-
     void performMining();
 
     // Function definitions
     inline double multiply(double k);
-
     inline double extract_32_digits(double k);
-
     inline double select_and_do_operation(double k, int seq);
-
     bool isValidHash(const std::string& hash, int requiredLeadingZeros);
-} // namespace SPHINXPoW
+}
 
-#endif // POW_HPP
+#endif // SPHINX_MINING_HPP
