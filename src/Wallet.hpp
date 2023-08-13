b6f90899bda9bb2ab16527cd123edffdd5fa9307
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-08-13 20:14:26 +0700

    Update Wallet.hpp
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- src/Wallet.hpp
+++ src/Wallet.hpp
@@ -8,10 +8,12 @@
 
 #pragma once
 
-#include <random>
-#include <string>
+#include <iostream>
 #include <vector>
+#include <random>
+#include <ctime>
 
+using json = nlohmann::json;
 
 namespace SPHINXWallet {
 
@@ -24,39 +26,48 @@ namespace SPHINXWallet {
     };
 
     class Wallet {
-    private:
-        WalletInfo walletInfo_;
-        std::string walletAddress_;
-        std::string privateKey_;
-        std::string passphrase_;
-
-        std::string generateWalletAddress();
-        std::string generatePrivateKey();
-        std::string encryptPassphrase(const std::string& passphrase);
-        std::string decryptPassphrase(const std::string& encryptedPassphrase);
-        std::string serializeData(const SPHINXDb::Data& data);
-        SPHINXDb::Data deserializeData(const std::string& serializedData);
-        void saveWalletInfo(const std::string& walletAddress, const std::string& privateKey, const std::string& encryptedPassphrase);
-        void loadWalletInfo();
-
     public:
         Wallet();
-        void initiateTransaction(const std::string& recipientAddress, double amount);
-        void createWallet();
-        void accessWallet(const std::string& walletAddress, const std::string& passphrase);
-        void signOut();
-        WalletInfo getWalletInfo() const;
-        bool sendTransaction(const std::string& recipient, double amount);
-        double getWalletBalance();
-        std::vector<SPHINXTrx::Transaction> getTransactionHistory();
-        std::string getPublicKey();
-        bool validateAddress(const std::string& address);
-        void importWallet(const std::string& walletData);
-        std::string exportWallet();
-    };
 
+        void generateAccount();
+        void getAccountBalance();
+        void sendTransaction(const std::string& recipientAddress, double amount);
+        void getTransactionHistory();
+        void createToken(const std::string& tokenName, const std::string& tokenSymbol);
+        void transferToken(const std::string& recipientAddress, const std::string& tokenSymbol, double amount);
+        void createWallet();
+        void initiateTransaction(const std::string& recipientAddress, double amount);
+
+        std::string requestDecryption(const std::string& encryptedData);
+        std::string generateSmartContractAddress(const std::string& publicKey, const std::string& contractName);
+        std::string generateRandomWord(std::vector<std::string>& wordList);
+
+    private:
+        std::string generateWalletAddress();
+        std::string generatePrivateKey();
+        void saveWalletInfo(const std::string& walletAddress, const std::string& privateKey, const std::string& encryptedPassphrase);
+        void loadWalletInfo();
+        std::string serializeData(const SPHINXDb::Data& data);
+        SPHINXDb::Data deserializeData(const std::string& serializedData);
+
+        std::string encryptPassphrase(const std::string& passphrase);
+        std::string decryptPassphrase(const std::string& encryptedPassphrase);
+        double fetchAccountBalance(const std::string& address);
+        std::vector<SPHINXTrx::Transaction> fetchTransactionHistory(const std::string& address);
+        bool isValidPassphrase(const std::string& passphrase);
+        bool isWalletAddressInUse(const std::string& address);
+        double loadBalance();
+        std::vector<SPHINXTrx::Transaction> loadTransactionHistory();
+        std::string getPublicKey();
+
+        std::string walletAddress_;
+        std::string privateKey_;
+        std::string encryptedPassphrase_;
+        double balance_;
+        std::vector<SPHINXTrx::Transaction> transactions_;
+        std::string passphrase_; // Add a member variable for passphrase
+        WalletInfo walletInfo_; // Add a member variable for wallet information
+    };
 } // namespace SPHINXWallet
 
-#endif /* WALLET_HPP */
-
-
+#endif // WALLET_HPP
